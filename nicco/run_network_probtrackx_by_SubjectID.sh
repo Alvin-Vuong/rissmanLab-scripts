2f160de4c55c979700672c49eebb4174db15f749
Alvin Vuong <alvin.t.vuong@ucla.edu>
2015-07-20 16:47:35 -0700

    probtrackx dir change Gordon

--- nicco/run_network_probtrackx_by_SubjectID.sh
+++ nicco/run_network_probtrackx_by_SubjectID.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
-#############################################################################
+###################################################################################
 # run_network_probtrackx_by_SubjectID.sh
 #
 # This script runs probtrack jobs for selected subjects' seeds on the grid
 # and stores the results in:
 #
 # ~/Nicco/NIQ/EXPANSION/Probtrack_Subject_Specific/{SubjectID}/From_{Seed#}/
+# (^ For Petersen ROIs)
+#
+# ~/Nicco/NIQ/EXPANSION/Probtrack_Subject_Specific/{SubjectID}/Gordon/From_{Seed#}/
+# (^ For Gordon ROIs)
 #
 # This is the newer version of the code and uses a user input prompt to 
 # ask for the starting subject ID, and automates the process of finding 
@@ -16,7 +20,7 @@
 #
 # Bug: At subject # input, you can input a partial match of the first subject
 #      and it will return the line number: 1.
-#############################################################################
+###################################################################################
 
 # Set paths
 top_path="/space/raid6/data/rissman/Nicco/NIQ/EXPANSION"
@@ -52,13 +56,13 @@ then
       # Send probtrack job to grid
       echo "Launching Subject ${j} With Seed ${f} to the grid!"
       
-      sge qsub probtrackx2  -x $seed  -l --onewaycondition --omatrix1 -c 0.2 -S 2000 --steplength=0.5 -P 5000 --fibthresh=0.01 --distthresh=0.0 --sampvox=0.0 --forcedir --opd -s "${BEDPOST_FOLDER}/merged" -m "${BEDPOST_FOLDER}/nodif_brain_mask.nii.gz"  --dir="${top_path}/Probtrack_Subject_Specific/Gordon/${j: 0: -1}/From_${f}" --targetmasks=$target_list --s2tastext --os2t
+      sge qsub probtrackx2  -x $seed  -l --onewaycondition --omatrix1 -c 0.2 -S 2000 --steplength=0.5 -P 5000 --fibthresh=0.01 --distthresh=0.0 --sampvox=0.0 --forcedir --opd -s "${BEDPOST_FOLDER}/merged" -m "${BEDPOST_FOLDER}/nodif_brain_mask.nii.gz"  --dir="${top_path}/Probtrack_Subject_Specific/${j: 0: -1}/Gordon/From_${f}" --targetmasks=$target_list --s2tastext --os2t
       
       # Every 5 jobs, sleep 2 mins
       if [ $(($f % 5)) == 0 ]
       then
-	    echo "Sleeping for 2 minutes to prevent grid clogging"
-	    sleep 2m
+	echo "Sleeping for 2 minutes to prevent grid clogging"
+	sleep 2m
       fi
     done
   done
@@ -103,13 +107,13 @@ then
       # Send probtrack job to grid
       echo "Launching Subject ${j} With Seed ${f} to the grid!"
       
-      sge qsub probtrackx2  -x $seed  -l --onewaycondition --omatrix1 -c 0.2 -S 2000 --steplength=0.5 -P 5000 --fibthresh=0.01 --distthresh=0.0 --sampvox=0.0 --forcedir --opd -s "${BEDPOST_FOLDER}/merged" -m "${BEDPOST_FOLDER}/nodif_brain_mask.nii.gz"  --dir="${top_path}/Probtrack_Subject_Specific/Gordon/${j: 0: -1}/From_${f}" --targetmasks=$target_list --s2tastext --os2t
+      sge qsub probtrackx2  -x $seed  -l --onewaycondition --omatrix1 -c 0.2 -S 2000 --steplength=0.5 -P 5000 --fibthresh=0.01 --distthresh=0.0 --sampvox=0.0 --forcedir --opd -s "${BEDPOST_FOLDER}/merged" -m "${BEDPOST_FOLDER}/nodif_brain_mask.nii.gz"  --dir="${top_path}/Probtrack_Subject_Specific/${j: 0: -1}/Gordon/From_${f}" --targetmasks=$target_list --s2tastext --os2t
       
       # Every 5 jobs, sleep 2 mins
       if [ $(($f % 5)) == 0 ]
       then
-	    echo "Sleeping for 2 minutes to prevent grid clogging"
-	    sleep 2m
+	echo "Sleeping for 2 minutes to prevent grid clogging"
+	sleep 2m
       fi
     done
   done
