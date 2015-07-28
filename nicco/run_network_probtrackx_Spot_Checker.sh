87ccb019ee152f3e8650c1bd995c3f49189b85d8
Alvin Vuong <alvin.t.vuong@ucla.edu>
2015-07-28 15:32:49 -0700

    Spot_Checker script implemented

--- nicco/run_network_probtrackx_Spot_Checker.sh
+++ nicco/run_network_probtrackx_Spot_Checker.sh
@@ -14,6 +14,8 @@
 # This checks both Gordon and Petersen seeds to see if they are complete.
 # Alter the code if you want only one type to be checked.
 #
+# *(Currently set to check only Petersen seeds)*
+#
 # Bug: At subject # input, you can input a partial match of the first subject
 #      and it will return the line number: 1.  Need regex for non-partial matches.
 ###################################################################################
@@ -34,111 +36,120 @@ subj_dirs=$(ls -d [0-9]*)
 # Loop through all subjects
 for j in $subj_dirs
 do
-  BEDPOST_FOLDER="/space/raid6/data/rissman/Nicco/NIQ/EXPANSION/Bedpost_Analysis/${j: 0: -1}.bedpostX"
+  BEDPOST_FOLDER="/space/raid6/data/rissman/Nicco/NIQ/EXPANSION/Bedpost_Analysis/${j}.bedpostX"
   
-  # Move into subject folder
+  # Move into subject folder (Petersen seeds directory)
+  echo "Moving to Subject $j's Petersen seeds"
   cd $j
   
   # Check if all Petersen seed folders exist
   if [ $(ls -d1 F* | wc -l) -ne 264 ]
   then
-    # Loop through Petersen seeds
-    petersen_seeds=$(ls -d F*/)
-    for ((p = 1; p <= 264; p++))
+    # Grab finished Petersen seeds
+    petersen_seeds=$(ls -d F*)
+
+    # Loop through potential seeds
+    for ((ps = 1; ps <= 264; ps++))
     do
-      # Find a missing seed
+      # Loop through finished seeds
+      foundP=0
+      for p in $petersen_seeds
+      do
+        # Check if seed is finished
+        if [ "$ps" = "${p:5}" ]
+        then
+          foundP=1
+          break
+        fi
+      done
       
+      # Seed not found among finished seeds
+      if [ $foundP -eq 0 ]
+      then
+        # Resubmit this seed to the grid
+        echo "Relaunching Subject $j, Petersen Seed $ps on the grid..."
+
+        # Set variables
+        seed="${mask_path}/${j}_Petersen_${ps}.nii.gz"
+        target_list="${targets_path}/${j}_Petersen_From_${ps}.txt"
+      
+        # Send probtrack job to grid
+#        sge qsub probtrackx2  -x $seed  -l --onewaycondition --omatrix1 -c 0.2 -S 2000 --steplength=0.5 -P 5000 --fibthresh=0.01 --distthresh=0.0 --sampvox=0.0 --forcedir --opd -s "${BEDPOST_FOLDER}/merged" -m "${BEDPOST_FOLDER}/nodif_brain_mask.nii.gz"  --dir="${top_path}/Probtrack_Subject_Specific/${j}/From_${ps}" --targetmasks=$target_list --s2tastext --os2t
+
+        # TODO: Implement sleep counter
+        # Increment sleep counter
+#        f=$(( f + 1 )
+      
+        # Every 5 jobs, sleep 2 mins
+#        if [ $(($f % 5)) == 0 ]
+#        then
+#	   echo "Sleeping for 2 minutes to prevent grid clogging"
+#	   sleep 2m
+#        fi
+
+      else
+        # Seed is already complete
+        echo "Subject $j, Petersen Seed $ps is already complete."
+      fi
     done
   fi
+  
+  # Move to subject's Gordon seeds directory
+  echo "Moving to Subject $j's Gordon seeds"
+  cd Gordon
   
   # Check if all Gordon seed folders exist
-  cd Gordon
   if [ $(ls -d1 F* | wc -l) -ne 333 ]
   then
-    # Loop through Gordon seeds
-    gordon_seeds=$(ls -d F*/)
-    for ((g = 1; g <= 333; g++))
+    # Grab finished Gordon seeds
+    gordon_seeds=$(ls -d F*)
+
+    # Loop through potential seeds
+    for ((gs = 1; gs <= 333; gs++))
     do
-      # Find a missing seed
+      # Loop through finished seeds
+      foundG=0
+      for g in $gordon_seeds
+      do
+        # Check if seed is finished
+        if [ "$gs" = "${g:5}" ]
+        then
+          foundG=1
+          break
+        fi
+      done
+
+      # Seed not found among finished seeds
+      if [ $foundG -eq 0 ]
+      then
+        # Resubmit this seed onto the grid
+        echo "Relaunching Subject $j, Gordon Seed $gs on the grid..."
+
+        # Set variables
+        seed="${mask_path}/${j}_Gordon_${gs}.nii.gz"
+        target_list="${targets_path}/${j}_Gordon_From_${gs}.txt"
       
+        # Send probtrack job to grid
+#        sge qsub probtrackx2  -x $seed  -l --onewaycondition --omatrix1 -c 0.2 -S 2000 --steplength=0.5 -P 5000 --fibthresh=0.01 --distthresh=0.0 --sampvox=0.0 --forcedir --opd -s "${BEDPOST_FOLDER}/merged" -m "${BEDPOST_FOLDER}/nodif_brain_mask.nii.gz"  --dir="${top_path}/Probtrack_Subject_Specific/${j}/Gordon/From_${gs}" --targetmasks=$target_list --s2tastext --os2t
+        
+        # TODO: Implement sleep counter
+        # Increment sleep counter
+#        f=$(( f + 1))
+
+        # Every 5 jobs, sleep 2 mins
+#        if [ $(($f % 5)) == 0 ]
+#        then
+#          echo "Sleeping for 2 minutes to prevent grid clogging"
+#          sleep 2m
+#        fi
+
+      else
+        # Seed is already complete
+        echo "Subject $j, Gordon Seed $gs is already complete."
+      fi
     done
   fi
   
+  # Move back to subjects directory
+  cd $save_top_path
 done
-
-
-
-
-
-  # Loop through Petersen seeds
-  for ((f = 1; f <= 264; f++))
-    do
-      
-      # Set variables
-      seed="${mask_path}/${j: 0: -1}_Petersen_${f}.nii.gz"
-      target_list="${targets_path}/${j: 0: -1}_Petersen_From_${f}.txt"
-      
-      # Send probtrack job to grid
-      echo "Launching Subject ${j} With Seed ${f} to the grid!"
-      
-      sge qsub probtrackx2  -x $seed  -l --onewaycondition --omatrix1 -c 0.2 -S 2000 --steplength=0.5 -P 5000 --fibthresh=0.01 --distthresh=0.0 --sampvox=0.0 --forcedir --opd -s "${BEDPOST_FOLDER}/merged" -m "${BEDPOST_FOLDER}/nodif_brain_mask.nii.gz"  --dir="${top_path}/Probtrack_Subject_Specific/${j: 0: -1}/Gordon/From_${f}" --targetmasks=$target_list --s2tastext --os2t
-      
-      # Every 5 jobs, sleep 2 mins
-      if [ $(($f % 5)) == 0 ]
-      then
-	    echo "Sleeping for 2 minutes to prevent grid clogging"
-	    sleep 2m
-      fi
-    done
-  done
-
-elif [ $run_all == 'n' ]
-then
-  # Select certain subjects (ask for starting subject ID)
-  ls -d1 [0-9]* > subjs.txt
-  selected=""
-  while [ "$selected" == "" ]
-  do
-    echo "Type the subject ID that you want to run, followed by [ENTER]:"
-    read subject
-    
-    # Retrieve line number of subject
-    selected=$(grep -n -m 1 $subject subjs.txt | cut -f1 -d: )
-    echo $selected
-    if [ "$selected" == "" ]
-    then
-      echo "Subject not found."
-    fi
-  done
-
-  # Use line # of subject * 8 - 8 to get the starting index...
-  index=$(( selected * 8 - 8 ))
-  array=("${subj_dirs[@]:$index}")
-  
-  # Loop through selected subjects
-  for j in $array
-  do
-    echo $j
-    BEDPOST_FOLDER="/space/raid6/data/rissman/Nicco/NIQ/EXPANSION/Bedpost_Analysis/${j: 0: -1}.bedpostX"
-	
-    # Loop through seeds
-    for ((f = 1; f <= 333; f++))
-    do
-
-      # Set variables
-      seed="${mask_path}/${j: 0: -1}_Gordon_${f}.nii.gz"
-      target_list="${targets_path}/${j: 0: -1}_Gordon_From_${f}.txt"
-      
-      # Send probtrack job to grid
-      echo "Launching Subject ${j} With Seed ${f} to the grid!"
-      
-      sge qsub probtrackx2  -x $seed  -l --onewaycondition --omatrix1 -c 0.2 -S 2000 --steplength=0.5 -P 5000 --fibthresh=0.01 --distthresh=0.0 --sampvox=0.0 --forcedir --opd -s "${BEDPOST_FOLDER}/merged" -m "${BEDPOST_FOLDER}/nodif_brain_mask.nii.gz"  --dir="${top_path}/Probtrack_Subject_Specific/${j: 0: -1}/Gordon/From_${f}" --targetmasks=$target_list --s2tastext --os2t
-      
-      # Every 5 jobs, sleep 2 mins
-      if [ $(($f % 5)) == 0 ]
-      then
-	echo "Sleeping for 2 minutes to prevent grid clogging"
-	sleep 2m
-      fi
-    done
-  done
