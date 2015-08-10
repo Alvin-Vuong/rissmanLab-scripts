9d449fffb76e9928fb34fc902586ba9b7dbe2177
Alvin Vuong <alvin.t.vuong@ucla.edu>
2015-08-09 17:49:56 -0700

    target_compressor.sh

---
+++ nicco/target_compressor.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+###################################################################################
+# target_compressor.sh
+#
+# This script compresses all target files within a subject folder.
+# Currently Subject ID is alterable.
+#
+# TODO: Allow user input of Subject ID.
+#       Loop through subject range.
+###################################################################################
+
+# Set paths
+top_path="/space/raid6/data/rissman/Nicco/NIQ/EXPANSION/Probtrack_Subject_Specific"
+
+# Move into subject directory and grab seed folders
+cd "${top_path}/100307"
+seed_dirs=$(ls -d From*)
+
+# Loop through all seeds
+for j in $seed_dirs
+do
+  # Move into seed folder
+  echo "Seed: $j"
+  cd $j
+  
+  # Compress all target files
+  gzip seeds_to_*
+
+  # Move back to subject directory
+  cd "${top_path}/100307"
+done
