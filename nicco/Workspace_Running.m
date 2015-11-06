31bb5c09e5d4800e104580de3f062578f1283018
Alvin Vuong <alvin.t.vuong@ucla.edu>
2015-11-05 17:40:21 -0800

    Sort works and pen = 1 added

--- nicco/Workspace_Running.m
+++ nicco/Workspace_Running.m
@@ -1,46 +1,235 @@
 % Set paths and dependencies
-toolboxRoot=['/space/raid6/data/rissman/Nicco/MATLAB_PATH/'];
+toolboxRoot = '/space/raid6/data/rissman/Nicco/MATLAB_PATH/';
 addpath(genpath(toolboxRoot))
-toolboxRoot=['/space/raid6/data/rissman/Nicco/NIQ/Scripts'];
+toolboxRoot = '/space/raid6/data/rissman/Nicco/NIQ/Scripts';
 addpath(genpath(toolboxRoot))
 
-% Declare types and vars
-internetwork_connection_types={'amXY_wX_wY', 'amXY_wX', 'amXY_wY', 'amXY', 'aoXY_wX_wY', 'aoXY_wX', 'aoXY_wY', 'aoXY'};
+% Get network info
+network_indices_path = '/space/raid6/data/rissman/Nicco/NIQ/Network_Indices/';
+load([network_indices_path 'Petersen_Networks.mat']);
+networks = fieldnames(Petersen_Networks);
+
+% Declare connection types
+internetwork_mutual_connection_types = {'amXY_wX_wY', 'amXY_wX', 'amXY_wY', 'amXY'};
+internetwork_oneway_connection_types = {'aoXY_wX_wY', 'aoXY_wX', 'aoXY_wY', 'aoXY'};
 intranetwork_connection_types={'wX'};
 
-behavs_of_interest={'PMAT24_A_CR'};
+% Choose behavioral variables of interest
+behavs_of_interest = {'PMAT24_A_CR'};
+
+% Choose networks of interest
+%Petersen_Networks.Default_Mode
+%Petersen_Networks.Cingulo_Opercular
+%Petersen_Networks.Salience
+%Petersen_Networks.Salience_and_Cingulo_Opercular
+
 Network_1='Default_Mode';
 Network_2='Cingulo_Opercular';
 
-% Ridge
-for c=2:size(internetwork_connection_types,2)
-    for b=1:size(behavs_of_interest,2)
-        fprintf('Starting Ridge: %s\n', internetwork_connection_types{c});
-        try NIQ_full_Ridge(behavs_of_interest{b},internetwork_connection_types{c},Network_1,Network_2); 
+%%%%%%%%%%%%%%%%%%%%%%%%%%% Intranetwork Ridge SVR Lasso %%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Intranetwork Ridge only
+for i = 1:size(intranetwork_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting Ridge: %s %s\n', intranetwork_connection_types{i}, Network_1);
+        try NIQ_full_Ridge(behavs_of_interest{j},intranetwork_connection_types{i},Network_1);
+        catch
+            continue
+        end
+    end
+end
+for i = 1:size(intranetwork_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting Ridge: %s %s\n', intranetwork_connection_types{i}, Network_2);
+        try NIQ_full_Ridge(behavs_of_interest{j},intranetwork_connection_types{i},Network_2);
         catch
             continue
         end
     end
 end
 
-% SVR
-for c=2:size(internetwork_connection_types,2)
-    for b=1:size(behavs_of_interest,2)
-        fprintf('Starting SVR: %s\n', internetwork_connection_types{c});
-        try NIQ_full_SVR(behavs_of_interest{b},internetwork_connection_types{c},Network_1,Network_2); 
+% Pen = 1
+for i = 1:size(intranetwork_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting Ridge: %s %s\n', intranetwork_connection_types{i}, Network_1);
+        try NIQ_full_Ridge_pen1(behavs_of_interest{j},intranetwork_connection_types{i},Network_1);
+        catch
+            continue
+        end
+    end
+end
+for i = 1:size(intranetwork_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting Ridge: %s %s\n', intranetwork_connection_types{i}, Network_2);
+        try NIQ_full_Ridge_pen1(behavs_of_interest{j},intranetwork_connection_types{i},Network_2);
         catch
             continue
         end
     end
 end
 
-% Lasso
-for c=2:size(internetwork_connection_types,2)
-    for b=1:size(behavs_of_interest,2)
-        fprintf('Starting Lasso: %s\n', internetwork_connection_types{c});
-        try NIQ_full_Lasso(behavs_of_interest{b},internetwork_connection_types{c},Network_1,Network_2); 
+%{
+
+% Intranetwork SVR Only
+for i = 1:size(intranetwork_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting SVR: %s %s\n', intranetwork_connection_types{i}, Network_1);
+        try NIQ_full_SVR(behavs_of_interest{j},intranetwork_connection_types{i},Network_1);
         catch
             continue
         end
     end
 end
+for i = 1:size(intranetwork_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting SVR: %s %s\n', intranetwork_connection_types{i}, Network_2);
+        try NIQ_full_SVR(behavs_of_interest{j},intranetwork_connection_types{i},Network_2);
+        catch
+            continue
+        end
+    end
+end
+
+% Intranetwork Lasso Only
+for i = 1:size(intranetwork_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting Lasso: %s %s\n', intranetwork_connection_types{i}, Network_1);
+        try NIQ_full_Lasso(behavs_of_interest{j},intranetwork_connection_types{i},Network_1);
+        catch
+            continue
+        end
+    end
+end
+for i = 1:size(intranetwork_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting Lasso: %s %s\n', intranetwork_connection_types{i}, Network_2);
+        try NIQ_full_Lasso(behavs_of_interest{j},intranetwork_connection_types{i},Network_2);
+        catch
+            continue
+        end
+    end
+end
+
+%}
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%% Internetwork Ridge SVR Lasso %%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Internetwork Ridge Only
+for i = 1:size(internetwork_mutual_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting Ridge: %s %s %s\n', internetwork_mutual_connection_types{i}, Network_1, Network_2);
+        try NIQ_full_Ridge(behavs_of_interest{j},internetwork_mutual_connection_types{i},Network_1,Network_2); 
+        catch
+            continue
+        end
+    end
+end
+for i = 1:size(internetwork_oneway_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting Ridge: %s %s %s\n', internetwork_oneway_connection_types{i}, Network_1, Network_2);
+        try NIQ_full_Ridge(behavs_of_interest{j},internetwork_oneway_connection_types{i},Network_1,Network_2); 
+        catch
+            continue
+        end
+    end
+end
+for i = 1:size(internetwork_oneway_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting Ridge: %s %s %s\n', internetwork_oneway_connection_types{i}, Network_2, Network_1);
+        try NIQ_full_Ridge(behavs_of_interest{j},internetwork_oneway_connection_types{i},Network_2,Network_1); 
+        catch
+            continue
+        end
+    end
+end
+
+% Pen = 1
+for i = 1:size(internetwork_mutual_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting Ridge: %s %s %s\n', internetwork_mutual_connection_types{i}, Network_1, Network_2);
+        try NIQ_full_Ridge_pen1(behavs_of_interest{j},internetwork_mutual_connection_types{i},Network_1,Network_2); 
+        catch
+            continue
+        end
+    end
+end
+for i = 1:size(internetwork_oneway_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting Ridge: %s %s %s\n', internetwork_oneway_connection_types{i}, Network_1, Network_2);
+        try NIQ_full_Ridge_pen1(behavs_of_interest{j},internetwork_oneway_connection_types{i},Network_1,Network_2); 
+        catch
+            continue
+        end
+    end
+end
+for i = 1:size(internetwork_oneway_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting Ridge: %s %s %s\n', internetwork_oneway_connection_types{i}, Network_2, Network_1);
+        try NIQ_full_Ridge_pen1(behavs_of_interest{j},internetwork_oneway_connection_types{i},Network_2,Network_1); 
+        catch
+            continue
+        end
+    end
+end
+
+%{
+
+% Internetwork SVR Only
+for i = 1:size(internetwork_mutual_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting SVR: %s %s %s\n', internetwork_mutual_connection_types{i}, Network_1, Network_2);
+        try NIQ_full_SVR(behavs_of_interest{j},internetwork_mutual_connection_types{i},Network_1,Network_2); 
+        catch
+            continue
+        end
+    end
+end
+for i = 1:size(internetwork_oneway_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting SVR: %s %s %s\n', internetwork_oneway_connection_types{i}, Network_1, Network_2);
+        try NIQ_full_SVR(behavs_of_interest{j},internetwork_oneway_connection_types{i},Network_1,Network_2); 
+        catch
+            continue
+        end
+    end
+end
+for i = 1:size(internetwork_oneway_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting SVR: %s %s %s\n', internetwork_oneway_connection_types{i}, Network_2, Network_1);
+        try NIQ_full_SVR(behavs_of_interest{j},internetwork_oneway_connection_types{i},Network_2,Network_1); 
+        catch
+            continue
+        end
+    end
+end
+
+% Internetwork Lasso Only
+for i = 1:size(internetwork_mutual_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting Lasso: %s %s %s\n', internetwork_mutual_connection_types{i}, Network_1, Network_2);
+        try NIQ_full_Lasso(behavs_of_interest{j},internetwork_mutual_connection_types{i},Network_1,Network_2); 
+        catch
+            continue
+        end
+    end
+end
+for i = 1:size(internetwork_oneway_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting Lasso: %s %s %s\n', internetwork_oneway_connection_types{i}, Network_1, Network_2);
+        try NIQ_full_Lasso(behavs_of_interest{j},internetwork_oneway_connection_types{i},Network_1,Network_2); 
+        catch
+            continue
+        end
+    end
+end
+for i = 1:size(internetwork_oneway_connection_types,2)
+    for j = 1:size(behavs_of_interest,2)
+        fprintf('Starting Lasso: %s %s %s\n', internetwork_oneway_connection_types{i}, Network_2, Network_1);
+        try NIQ_full_Lasso(behavs_of_interest{j},internetwork_oneway_connection_types{i},Network_2,Network_1); 
+        catch
+            continue
+        end
+    end
+end
+
+%}
--- nicco/output_results.sh
+++ nicco/output_results.sh
@@ -1,6 +1,66 @@
+#!/bin/sh
+
+# Sorted output
+
+# Grab all .txt files
 FILES=*.txt
+
+# Init arrays and counter
+FILENAMES[0]=""
+VALUES[0]=""
+ABSVALUES[0]=""
+i=0
+
+# Fill arrays
 for f in $FILES
 do
-  echo "$f"
-  cat $f
+  # Store each filename in an array
+  FILENAMES[$i]=$f
+
+  # Store file's r-value in an array
+  VALUES[$i]=$(awk 'NR==2' $f)
+
+  # Store absolute value of r in an array
+  ABSVALUES[$i]=$(echo ${VALUES[i]} | tr -d -)
+
+  # Increment counter
+  i=$((i+1))
+done
+
+# Sort arrays
+curr=0
+i=0
+for f in $FILES
+do
+  currABS=${ABSVALUES[i]}
+  currVAL=${VALUES[i]}
+  currFILE=${FILENAMES[i]}
+  for (( j = 0; j < i; j++ ))
+  do
+    #TF=$(echo "$currABS < ${ABSVALUES[j]}" | bc)
+    TF=$(echo "$currVAL < ${VALUES[j]}" | bc)
+    if [ $TF -eq 1 ]
+    then
+      for (( k = i-1; k > j-1; k-- ))
+      do
+	ABSVALUES[$((k+1))]=${ABSVALUES[k]}
+	VALUES[$((k+1))]=${VALUES[k]}
+	FILENAMES[$((k+1))]=${FILENAMES[k]}
+      done
+      ABSVALUES[$j]=$currABS
+      VALUES[$j]=$currVAL
+      FILENAMES[$j]=$currFILE
+      break
+    fi
+  done
+  i=$((i+1))
+done
+
+# Output result
+i=0
+for f in $FILES
+do
+  echo "${FILENAMES[i]}"
+  echo "${VALUES[i]}"
+  i=$((i+1))
 done
