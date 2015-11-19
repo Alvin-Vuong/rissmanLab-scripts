19ff46333ed56d6fcd8be4fc998a54df271d3c4c
Alvin Vuong <alvin.t.vuong@ucla.edu>
2015-11-18 17:22:11 -0800

    probtrackx_max with Gordon

--- nicco/probtrackx_max.sh
+++ nicco/probtrackx_max.sh
@@ -20,10 +20,6 @@
 #         the specified job limit has been reached. Then sleeps for 15 minutes
 #         before submitting more jobs. (Constant FUNC load).
 #
-# Note on FUNC efficiency:
-#   Sleeping for 2 minutes per 5 subjects is not slow enough for the SGE.
-#   Just keep an eye on FUNC SGE load with 'sge_qstat_checker.sh'.
-#
 # Bug: At subject # input, you can input a partial match of the first subject
 #      and it will return the line number: 1.  Need regex for non-partial matches.
 ###################################################################################
@@ -48,146 +44,198 @@ subjNum=0
 jumper=0
 f=0
 
+# Ask for Petersen or Gordon
+echo -n "Enter 'P' for Petersen or 'G' for Gordon parcellation: "
+read type
+
 # Ask for starting subject # and # of subjects to run
 echo -n "Enter starting subject #: "
 read start
 echo -n "Enter # of subjects to run: "
 read amt
 
-# Loop through all subjects
-for j in $subj_dirs
-do
-  # Only do ith to jth (j = i + N) subjects
-  jumper=$(( jumper + 1 ))
-  if [ $jumper -lt $start ]
-  then
-    continue
-  fi
-
-  # Only do N subjects
-  subjNum=$(( subjNum + 1 ))
-  if [ $subjNum -eq $(( amt + 1 )) ]
-  then
-    break
-  fi
-
-  # Check if subject is missing .bedpostX (from .txt list)
-  if grep -Fxq "$j" $missing_bedpostX_path
-  then
-    echo "Subject $j is missing .bedpostX"
-    continue
-  fi
-
-  BEDPOST_FOLDER="/space/raid6/data/rissman/Nicco/NIQ/EXPANSION/Bedpost_Analysis/${j}.bedpostX"
-  
-  # Move into subject folder (Petersen seeds directory)
-  echo "Moving to Subject #$jumper, ID: $j Petersen seeds"
-  cd $j
-
-  # Grab finished Petersen seeds
-  petersen_seeds=$(ls -d F*)
-  
-  # Loop through potential seeds
-  for ((ps = 1; ps <= 264; ps++))
+# Petersen
+if [ "$type" = "P" ]
+then
+  echo "Type: Petersen"
+  # Loop through all subjects
+  for j in $subj_dirs
   do
-    # Loop through finished seeds
-    foundP=0
-    for p in $petersen_seeds
-    do
-      # Check if seed folder exists
-      if [ "$ps" = "${p:5}" ]
-      then
-	# Check if fdt_paths exists
-	echo "Checking seed: $ps"
-	cd "From_${ps}"
-        if [ $(ls -d1 fdt_paths.* | wc -l) -eq 1 ]
-        then
-	  foundP=1
-	  break
-	fi
-	break
-      fi
-    done
-      
-    # Seed not found among finished seeds
-    if [ $foundP -eq 0 ]
+    # Only do ith to jth (j = i + N) subjects
+    jumper=$(( jumper + 1 ))
+    if [ $jumper -lt $start ]
     then
-      # Resubmit this seed to the grid
-      echo "Relaunching Subject #$jumper, ID: $j, Petersen Seed $ps on the grid..."
-      
-      # Set variables
-      seed="${mask_path}/${j}_Petersen_${ps}.nii.gz"
-      target_list="${targets_path}/${j}_From_${ps}.txt"
-      
-      # Check if FUNC SGE is overloaded
-      while [ $( sge qstat | awk '(NR>2){print $1}' | wc -l ) -ge $maxLoad ]
-      do
-	echo "Server is overloaded. Sleeping 15 minutes..."
-	sleep 15m
-      done
-
-      # Send probtrack job to grid
-      sge qsub probtrackx2  -x $seed  -l --onewaycondition --omatrix1 -c 0.2 -S 2000 --steplength=0.5 -P 5000 --fibthresh=0.01 --distthresh=0.0 --sampvox=0.0 --forcedir --opd -s "${BEDPOST_FOLDER}/merged" -m "${BEDPOST_FOLDER}/nodif_brain_mask.nii.gz"  --dir="${top_path}/Probtrack_Subject_Specific/${j}/From_${ps}" --targetmasks=$target_list --s2tastext --os2t
-
+      continue
     fi
-    cd "${save_top_path}/${j}"
-  done
-  
-  # Move to subject's Gordon seeds directory
-#  echo "Moving to Subject $j's Gordon seeds"
-#  cd Gordon
-  
-  # Check if all Gordon seed folders exist
-#  if [ $(ls -d1 F* | wc -l) -ne 333 ]
-#  then
-    # Grab finished Gordon seeds
-#    gordon_seeds=$(ls -d F*)
 
+    # Only do N subjects
+    subjNum=$(( subjNum + 1 ))
+    if [ $subjNum -eq $(( amt + 1 )) ]
+    then
+      break
+    fi
+
+    # Check if subject is missing .bedpostX (from .txt list)
+    if grep -Fxq "$j" $missing_bedpostX_path
+    then
+      echo "Subject $j is missing .bedpostX"
+      continue
+    fi
+
+    BEDPOST_FOLDER="/space/raid6/data/rissman/Nicco/NIQ/EXPANSION/Bedpost_Analysis/${j}.bedpostX"
+    
+    # Move into subject folder (Petersen seeds directory)
+    echo "Moving to Subject #$jumper, ID: $j Petersen seeds"
+    cd $j
+
+    # Grab finished Petersen seeds
+    petersen_seeds=$(ls -d F*)
+  
     # Loop through potential seeds
-#    for ((gs = 1; gs <= 333; gs++))
-#    do
+    for ((ps = 1; ps <= 264; ps++))
+    do
       # Loop through finished seeds
-#      foundG=0
-#      for g in $gordon_seeds
-#      do
-        # Check if seed is finished
-#        if [ "$gs" = "${g:5}" ]
-#        then
-#          foundG=1
-#          break
-#        fi
-#      done
-
-      # Seed not found among finished seeds
-#      if [ $foundG -eq 0 ]
-#      then
-        # Resubmit this seed onto the grid
-#        echo "Relaunching Subject $j, Gordon Seed $gs on the grid..."
-
-      # Set variables
-#        seed="${mask_path}/${j}_Gordon_${gs}.nii.gz"
-#        target_list="${targets_path}/${j}_Gordon_From_${gs}.txt"
+      foundP=0
+      for p in $petersen_seeds
+      do
+	# Check if seed folder exists
+        if [ "$ps" = "${p:5}" ]
+	then
+	  # Check if fdt_paths exists
+	  echo "Checking seed: $ps"
+	  cd "From_${ps}"
+          if [ $(ls -d1 fdt_paths.* | wc -l) -eq 1 ]
+          then
+	    foundP=1
+	    break
+	  fi
+	  break
+        fi
+      done
       
-      # Send probtrack job to grid
-#        sge qsub probtrackx2  -x $seed  -l --onewaycondition --omatrix1 -c 0.2 -S 2000 --steplength=0.5 -P 5000 --fibthresh=0.01 --distthresh=0.0 --sampvox=0.0 --forcedir --opd -s "${BEDPOST_FOLDER}/merged" -m "${BEDPOST_FOLDER}/nodif_brain_mask.nii.gz"  --dir="${top_path}/Probtrack_Subject_Specific/${j}/Gordon/From_${gs}" --targetmasks=$target_list --s2tastext --os2t
+      # Seed not found among finished seeds
+      if [ $foundP -eq 0 ]
+      then
+        # Resubmit this seed to the grid
+        echo "Relaunching Subject #$jumper, ID: $j, Petersen Seed $ps on the grid..."
         
-      # Increment sleep counter
-#        f=$(( f + 1))
+        # Set variables
+        seed="${mask_path}/${j}_Petersen_${ps}.nii.gz"
+        target_list="${targets_path}/${j}_From_${ps}.txt"
+      
+        # Check if FUNC SGE is overloaded
+        while [ $( sge qstat | awk '(NR>2){print $1}' | wc -l ) -ge $maxLoad ]
+        do
+	  echo "Server is overloaded. Sleeping 15 minutes..."
+	  sleep 15m
+        done
 
-      # Every 5 jobs, sleep 2 mins
-#        if [ $(($f % 5)) == 0 ]
-#        then
-#          echo "Sleeping for 2 minutes to prevent grid clogging"
-#          sleep 2m
-#        fi
+        # Send probtrack job to grid
+        sge qsub probtrackx2  -x $seed  -l --onewaycondition --omatrix1 -c 0.2 -S 2000 --steplength=0.5 -P 5000 --fibthresh=0.01 --distthresh=0.0 --sampvox=0.0 --forcedir --opd -s "${BEDPOST_FOLDER}/merged" -m "${BEDPOST_FOLDER}/nodif_brain_mask.nii.gz"  --dir="${top_path}/Probtrack_Subject_Specific/${j}/From_${ps}" --targetmasks=$target_list --s2tastext --os2t
 
-#       else
-      # Seed is already complete
-#        echo "Subject $j, Gordon Seed $gs is already complete."
-#      fi
-#    done
-#  fi
+      fi
+      cd "${save_top_path}/${j}"
+    done
+    # Move back to subjects directory
+    cd $save_top_path
+  done
+
+# Gordon
+elif [ "$type" = "G" ]
+then
+  echo "Type: Gordon"
+  # Loop through all subjects
+  for j in $subj_dirs
+  do
+    # Only do ith to jth (j = i + N) subjects
+    jumper=$(( jumper + 1 ))
+    if [ $jumper -lt $start ]
+    then
+      continue
+    fi
+
+    # Only do N subjects
+    subjNum=$(( subjNum + 1 ))
+    if [ $subjNum -eq $(( amt + 1 )) ]
+    then
+      break
+    fi
+
+    # Check if subject is missing .bedpostX (from .txt list)
+    if grep -Fxq "$j" $missing_bedpostX_path
+    then
+      echo "Subject $j is missing .bedpostX"
+      continue
+    fi
+
+    BEDPOST_FOLDER="/space/raid6/data/rissman/Nicco/NIQ/EXPANSION/Bedpost_Analysis/${j}.bedpostX"
+    
+    # Move into subject folder (Gordon seeds directory)
+    echo "Moving to Subject #$jumper, ID: $j Gordon seeds"
+    cd $j
+    
+    # Check if Gordon folder exists, if not, create it
+    if [ $(ls -d1 Gordon | wc -l) -eq 1 ]
+    then
+      cd Gordon
+    else
+      mkdir Gordon
+      cd Gordon
+    fi
+    
+    # Grab finished Gordon seeds
+    gordon_seeds=$(ls -d F*)
   
-  # Move back to subjects directory
-  cd $save_top_path
-done
+    # Loop through potential seeds
+    for ((ps = 1; ps <= 333; ps++))
+    do
+      # Loop through finished seeds
+      foundP=0
+      for p in $gordon_seeds
+      do
+	# Check if seed folder exists
+        if [ "$ps" = "${p:5}" ]
+	then
+	  # Check if fdt_paths exists
+	  echo "Checking seed: $ps"
+	  cd "From_${ps}"
+          if [ $(ls -d1 fdt_paths.* | wc -l) -eq 1 ]
+          then
+	    foundP=1
+	    break
+	  fi
+	  break
+        fi
+      done
+      
+      # Seed not found among finished seeds
+      if [ $foundP -eq 0 ]
+      then
+        # Resubmit this seed to the grid
+        echo "Relaunching Subject #$jumper, ID: $j, Gordon Seed $ps on the grid..."
+        
+        # Set variables
+        seed="${mask_path}/${j}_Gordon_${ps}.nii.gz"
+        target_list="${targets_path}/${j}_Gordon_From_${ps}.txt"
+      
+        # Check if FUNC SGE is overloaded
+        while [ $( sge qstat | awk '(NR>2){print $1}' | wc -l ) -ge $maxLoad ]
+        do
+	  echo "Server is overloaded. Sleeping 15 minutes..."
+	  sleep 15m
+        done
+
+        # Send probtrack job to grid
+        sge qsub probtrackx2  -x $seed  -l --onewaycondition --omatrix1 -c 0.2 -S 2000 --steplength=0.5 -P 5000 --fibthresh=0.01 --distthresh=0.0 --sampvox=0.0 --forcedir --opd -s "${BEDPOST_FOLDER}/merged" -m "${BEDPOST_FOLDER}/nodif_brain_mask.nii.gz"  --dir="${top_path}/Probtrack_Subject_Specific/${j}/Gordon/From_${ps}" --targetmasks=$target_list --s2tastext --os2t
+
+      fi
+      cd "${save_top_path}/${j}/Gordon"
+    done
+    # Move back to subjects directory
+    cd $save_top_path
+  done
+
+# Invalid type entered
+else
+  echo "Invalid Type!"
+fi
