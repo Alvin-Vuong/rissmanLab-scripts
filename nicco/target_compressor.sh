b96adc294a281937eb570e48cb5f6f1fd842203d
Alvin Vuong <alvin.t.vuong@ucla.edu>
2016-09-14 14:55:18 -0700

    Compressor modifications

--- nicco/target_compressor.sh
+++ nicco/target_compressor.sh
@@ -10,22 +10,41 @@
 ###################################################################################
 
 # Set paths
-top_path="/space/raid6/data/rissman/Nicco/NIQ/EXPANSION/Probtrack_Subject_Specific"
+#top_path="/space/raid6/data/rissman/Nicco/NIQ/EXPANSION/Probtrack_Subject_Specific"
+top_path="/Volumes/RissmanLab_5TB/Nicco/HCP/Alvin/Probtrack"
 
-# Move into subject directory and grab seed folders
-cd "${top_path}/100307"
-seed_dirs=$(ls -d From*)
-
-# Loop through all seeds
-for j in $seed_dirs
+# Infinite listen
+while true
 do
-  # Move into seed folder
-  echo "Seed: $j"
-  cd $j
-  
-  # Compress all target files
-  gzip -f seeds_to_*
 
-  # Move back to subject directory
-  cd "${top_path}/100307"
+  # Move into probtrack directory and grab subject folders
+  cd $top_path
+  subj_dirs=$(ls -d [0-9]*)
+
+  # Loop through all subjects
+  for i in $subj_dirs
+  do
+    # Move into subject directory and grab seed folders
+    echo "Subject: $i"
+    cd $i
+    seed_dirs=$(ls -d From_*)
+
+    # Loop through all seeds
+    for j in $seed_dirs
+    do
+      # Move into seed folder
+      echo "Seed: $j"
+      cd $j
+  
+      # Compress all .nii files
+      gzip -f *.nii
+
+      # Move back to subject directory
+      cd "${top_path}/${i}"
+    done
+
+    # Move back to probtrack directory
+    cd $top_path
+  done
+
 done
