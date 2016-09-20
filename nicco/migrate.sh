6efa2cb171dccb40778d67607fbed65d61bf9209
Alvin Vuong <alvin.t.vuong@ucla.edu>
2016-09-19 22:44:00 -0700

    Expect script added for automated password handling

--- nicco/migrate.sh
+++ nicco/migrate.sh
@@ -12,16 +12,65 @@
 list_dir="/space/raid6/data/rissman/Nicco/NIQ/Save/"
 probtrack_dir="/space/raid6/data/rissman/Nicco/NIQ/EXPANSION/Probtrack_Subject_Specific"
 bedpost_dir="/space/raid6/data/rissman/Nicco/NIQ/EXPANSION/Bedpost_Analysis"
+pass="#####"
 
 # Read in subjects and loop through them line-by-line
 while read line
 do
-  # Get subject folders and move to drive
+  # Get subject folders
   subject_probtrack="${probtrack_dir}/${line}"
   subject_bedpost="${bedpost_dir}/${line}"
-  scp -r avuong@dentate.psych.ucla.edu:${subject_probtrack} /Volumes/RissmanLab_5TB/Nicco/HCP/Alvin/Probtrack
-  scp -r avuong@dentate.psych.ucla.edu:${subject_bedpost} /Volumes/RissmanLab_5TB/Nicco/HCP/Alvin/Bedpost
-  scp -r avuong@dentate.psych.ucla.edu:${subject_bedpost}.bedpostX /Volumes/RissmanLab_5TB/Nicco/HCP/Alvin/Bedpost
+  
+  # Check if subject folder exists (if done already)
+  if [ ! -d "/Volumes/RissmanLab_5TB/Nicco/HCP/Alvin/Probtrack/${line}" ]
+  then
+
+    # Transfer to drive
+    rm temp
+    echo "
+    set timeout -1
+    spawn scp -r avuong@dentate.psych.ucla.edu:${subject_probtrack} /Volumes/RissmanLab_5TB/Nicco/HCP/Alvin/Probtrack
+    expect \"Password: \"
+    send \"${pass}\\r\"
+    expect \"$ \"
+    send \"exit\\r\"
+    " > temp
+    
+    /usr/bin/expect < temp
+
+  fi
+  
+  if [ ! -d "/Volumes/RissmanLab_5TB/Nicco/HCP/Alvin/Bedpost/${line}" ]
+  then
+    # Transfer to drive
+    rm temp
+    echo "
+    set timeout -1
+    spawn scp -r avuong@dentate.psych.ucla.edu:${subject_bedpost} /Volumes/RissmanLab_5TB/Nicco/HCP/Alvin/Bedpost
+    expect \"Password: \"
+    send \"${pass}\\r\"
+    expect \"$ \"
+    send \"exit\\r\"
+    " > temp
+    
+    /usr/bin/expect < temp
+  fi
+
+  if [ ! -d "/Volumes/RissmanLab_5TB/Nicco/HCP/Alvin/Bedpost/${line}.bedpostX" ]
+  then
+    # Transfer to drive
+    rm temp
+    echo "
+    set timeout -1
+    spawn scp -r avuong@dentate.psych.ucla.edu:${subject_bedpost}.bedpostX /Volumes/RissmanLab_5TB/Nicco/HCP/Alvin/Bedpost
+    expect \"Password: \"
+    send \"${pass}\\r\"
+    expect \"$ \"
+    send \"exit\\r\"
+    " > temp
+    
+    /usr/bin/expect < temp
+  fi
 
   # Remove subject folders
 
